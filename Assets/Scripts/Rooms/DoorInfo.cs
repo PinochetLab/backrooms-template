05bcc76242d54b604e2f1d5a774805b45dae9747
pinochet <a.vorobev1@g.nsu.ru>
2024-08-17 19:53:28 +0700

    made classes

---
+++ Assets/Scripts/Rooms/DoorInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInfo : MonoBehaviour
+{
+    public float centerX;
+    public MonoBehaviour mb;
+}
